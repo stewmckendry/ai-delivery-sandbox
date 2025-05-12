0c5622473948f6b3d3ec8bf412e9213346d46e63
stewmckendry <59027450+stewmckendry@users.noreply.github.com>
2025-05-12 14:48:35 -0400

    Update database.py to use AZURE_SQL_CONNECTION_STRING from environment

--- app/db/database.py
+++ app/db/database.py
@@ -2,6 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+# Use env variable for secure connection string
 DATABASE_URL = os.getenv("AZURE_SQL_CONNECTION_STRING")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
