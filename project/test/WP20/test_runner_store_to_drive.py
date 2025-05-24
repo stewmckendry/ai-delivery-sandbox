e3fa41f26b013a172b02b753ca077ec457fb1f5a
stewmckendry <59027450+stewmckendry@users.noreply.github.com>
2025-05-24 12:52:43 -0400

    register storetodrive tool

--- project/test/WP20/test_runner_store_to_drive.py
+++ project/test/WP20/test_runner_store_to_drive.py
@@ -1,4 +1,6 @@
 import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..')))
 from app.tools.tool_wrappers.storeToDrive import Tool
 
 if __name__ == "__main__":
