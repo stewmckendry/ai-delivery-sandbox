7a0412497e8fec24bd4f47363dd8e86e120d7209
stewmckendry <59027450+stewmckendry@users.noreply.github.com>
2025-05-23 23:55:10 -0400

    root path reference in test runner

--- project/test/WP18/test_runner_assemble_artifact.py
+++ project/test/WP18/test_runner_assemble_artifact.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Set path to project root so imports work
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../..")))
+
 from app.engines.planner_orchestrator import PlannerOrchestrator
 
 def run_successful_test():
