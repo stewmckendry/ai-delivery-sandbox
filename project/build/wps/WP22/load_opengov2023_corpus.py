627b2e492c271d095834bb65805da058ed8d6e66
stewmckendry <59027450+stewmckendry@users.noreply.github.com>
2025-05-26 12:15:46 -0400

    update

--- project/build/wps/WP22/load_opengov2023_corpus.py
+++ project/build/wps/WP22/load_opengov2023_corpus.py
@@ -1,4 +1,7 @@
 import requests
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../..")))
 import pdfplumber
 from app.tools.tool_wrappers.loadCorpus import Tool
 
