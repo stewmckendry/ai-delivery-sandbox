ca97658f987ec427fafa86787e2ad6e53227c1ed
stewmckendry <59027450+stewmckendry@users.noreply.github.com>
2025-05-27 14:45:42 -0400

    Wire up /profiles routes into routes/index.ts

--- super-secret-code/backend/routes/index.ts
+++ super-secret-code/backend/routes/index.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import authRoutes from './auth';
+import profilesRoutes from './profiles';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
+router.use('/profiles', profilesRoutes);
+
 router.get('/ping', (_req, res) => {
   res.send('pong');
 });
