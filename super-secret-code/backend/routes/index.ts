296870c8c1afc107bfb402f36614eab11f3325c6
stewmckendry <59027450+stewmckendry@users.noreply.github.com>
2025-05-27 14:44:27 -0400

    Wire up /auth routes into routes/index.ts

--- super-secret-code/backend/routes/index.ts
+++ super-secret-code/backend/routes/index.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
+import authRoutes from './auth';
 
 const router = Router();
 
+router.use('/auth', authRoutes);
 router.get('/ping', (_req, res) => {
   res.send('pong');
 });
