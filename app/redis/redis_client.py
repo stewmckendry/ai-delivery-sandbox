d4b696d3f84bd8dd7bf0298d79eb2ac09f20f31d
stewmckendry <59027450+stewmckendry@users.noreply.github.com>
2025-05-29 13:30:32 -0400

    manual update

--- app/redis/redis_client.py
+++ app/redis/redis_client.py
@@ -1,5 +1,10 @@
 import redis
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 redis_url = os.getenv("REDIS_URL")
+
+if not redis_url:
+	raise ValueError("REDIS_URL environment variable is not set")
 redis_client = redis.Redis.from_url(redis_url)
